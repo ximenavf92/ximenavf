ecd80ba106761329d413028b11be82bd19d683cd
Ximena Vila Ferral <hola@ximenavf.com>
2021-02-22 20:30:41 -0600

    add: back to top button and small sus animation (needs work)

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+// Back to Top Button
+let backToTopBtn = document.getElementById('backToTopBtn');
+let root = document.documentElement;
+let target = document.querySelector('.main-footer');
+let susmena = document.querySelector('.susmena');
+
+function handleScroll() {
+    let scrollTotal = root.scrollHeight - root.clientHeight;
+    
+    console.log('We scrolling!');
+    if ((root.scrollTop / scrollTotal) > 0) {
+        backToTopBtn.classList.add('show-btn');
+    } else {
+        backToTopBtn.classList.remove('show-btn');
+    }
+}
+
+function scrollToTop() {
+    root.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+    console.log('Scrolling to the tooop!');
+}
+
+
+function callback(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            susmena.classList.add('be-sus');
+        } else {
+            susmena.classList.remove('be-sus');
+        }
+    });
+}
+
+backToTopBtn.addEventListener('click', scrollToTop);
+document.addEventListener('scroll', handleScroll);
+
+// intersection observer to show back to top button
+let observer = new IntersectionObserver(callback);
+observer.observe(target);
